1e9feeec15f40ebcf1f8e3e25ab64ab9b6cbeb25
DevJLALW <115839414+DevJLALW@users.noreply.github.com>
2025-10-29 05:22:50 +0100

    feat: Add circuit breaker, retry, and time limiter to order placement
    - Integrated Resilience4j circuit breaker, retry, and time limiter in OrderController for inventory service calls.
    - Updated OrderService to support resilient inter-service communication.

--- order-service/src/main/java/com/shopverse/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/shopverse/orderservice/controller/OrderController.java
@@ -3,9 +3,15 @@ package com.shopverse.orderservice.controller;
 
 import com.shopverse.orderservice.dto.OrderRequest;
 import com.shopverse.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,10 +21,19 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Placed";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="inventory")
+    @Retry(name="inventory")
+    public CompletableFuture<ResponseEntity<String>> placeOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(() ->
+                ResponseEntity.status(HttpStatus.CREATED).body(orderService.placeOrder(orderRequest)));
 
     }
+
+    public CompletableFuture<ResponseEntity<String>> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() ->
+                ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Oops! Something went wrong!")
+        );
+    }
+
 }
